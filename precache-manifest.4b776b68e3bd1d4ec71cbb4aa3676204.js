fd93fed74ba847a501c059daad98aac76767e21c
Matas <matas.jarilinas@stud.vgtu.lt>
2019-09-05 15:07:07 +0300

    Updates

---
+++ precache-manifest.4b776b68e3bd1d4ec71cbb4aa3676204.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10187b96be7f84c99a9533c1d330879",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "bccf6c1b14d2f3b2f714",
+    "url": "/roboFriends/static/css/main.2c7dcd66.chunk.css"
+  },
+  {
+    "revision": "ec28cb1ac3a4d4a493c3",
+    "url": "/roboFriends/static/js/2.8628e6f4.chunk.js"
+  },
+  {
+    "revision": "bccf6c1b14d2f3b2f714",
+    "url": "/roboFriends/static/js/main.63606b63.chunk.js"
+  },
+  {
+    "revision": "0ed58bbeb75e4bf34113",
+    "url": "/roboFriends/static/js/runtime~main.4a8ff0e4.js"
+  }
+]);
