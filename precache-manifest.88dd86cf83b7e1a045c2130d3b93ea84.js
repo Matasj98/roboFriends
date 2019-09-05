0bea6080e931ec65b82eb5af9a833369102c9c1c
Matas <matas.jarilinas@stud.vgtu.lt>
2019-09-05 14:55:22 +0300

    a

---
+++ precache-manifest.88dd86cf83b7e1a045c2130d3b93ea84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81563aa99416fa71fa885163a000bc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcfa6e9fbdab14095301",
+    "url": "/static/css/main.2c7dcd66.chunk.css"
+  },
+  {
+    "revision": "ec28cb1ac3a4d4a493c3",
+    "url": "/static/js/2.8628e6f4.chunk.js"
+  },
+  {
+    "revision": "fcfa6e9fbdab14095301",
+    "url": "/static/js/main.787b6877.chunk.js"
+  },
+  {
+    "revision": "b1be32a13c4c82549f5d",
+    "url": "/static/js/runtime~main.01f2ffcb.js"
+  }
+]);
